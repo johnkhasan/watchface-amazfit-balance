715d77e902b7b9093e4d11c84a281e85f8d03016
javohirhasanov <javohirhasanov3303@gmail.com>
2026-07-09 10:00:44 +0500

    Implement code changes to enhance functionality and improve performance

--- watchface/balance/index.js
+++ watchface/balance/index.js
@@ -11,7 +11,7 @@ for (let i = 0; i <= 9; i++) {
 const HAND_POS_X = 231 // must match render_second_hand() output in scripts/generate_assets.py
 const HAND_POS_Y = 43
 
-let hourWidget, minuteWidget, secondHandWidget, timeSensor
+let hourWidget, minuteWidget, secondHandWidget, dateWidget, timeSensor
 
 function paint(hour, minute) {
   hourWidget.setProperty(hmUI.prop.TEXT, String(hour).padStart(2, '0'))
@@ -20,6 +20,11 @@ function paint(hour, minute) {
 
 function paintSecondHand(second) {
   secondHandWidget.setProperty(hmUI.prop.MORE, { angle: second * 6 })
+}
+
+function paintDate(day, month) {
+  const text = String(day).padStart(2, '0') + '.' + String(month).padStart(2, '0')
+  dateWidget.setProperty(hmUI.prop.MORE, { text: text })
 }
 
 WatchFace({
@@ -41,6 +46,22 @@ WatchFace({
       center_y: 240,
       src: 'second_hand.png',
       angle: 0,
+    })
+
+    dateWidget = hmUI.createWidget(hmUI.widget.TEXT, {
+      x: 0,
+      y: 28,
+      w: 480,
+      h: 34,
+      text: '00.00',
+      text_size: 32,
+      color: 0x999999,
+      align_h: hmUI.align.CENTER_H,
+      align_v: hmUI.align.CENTER_V,
+    })
+
+    dateWidget.addEventListener(hmUI.event.CLICK_DOWN, function () {
+      hmApp.startApp({ appid: 1, url: 'ScheduleCalScreen', native: true })
     })
 
     hourWidget = hmUI.createWidget(hmUI.widget.TEXT_IMG, {
@@ -69,9 +90,11 @@ WatchFace({
       if (!timeSensor) timeSensor = hmSensor.createSensor(hmSensor.id.TIME)
       paint(timeSensor.hour, timeSensor.minute)
       paintSecondHand(timeSensor.second)
+      paintDate(timeSensor.day, timeSensor.month)
 
       timeSensor.addEventListener(timeSensor.event.MINUTEEND, function () {
         paint(timeSensor.hour, timeSensor.minute)
+        paintDate(timeSensor.day, timeSensor.month)
       })
 
       timer.createTimer(0, 1000, function () {
@@ -82,6 +105,7 @@ WatchFace({
         resume_call: function () {
           paint(timeSensor.hour, timeSensor.minute)
           paintSecondHand(timeSensor.second)
+          paintDate(timeSensor.day, timeSensor.month)
         },
       })
     } catch (e) {
